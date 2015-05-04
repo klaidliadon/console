e21ef82ce85fe0dacf89a78f071e03709d6840d0
Alex Guerrieri <alex.guerrieri@gruppo4.it>
2015-05-04 11:13:31 +0200

    Testing hook release and string function execution

--- console.go
+++ console.go
@@ -2,13 +2,11 @@
 package console
 
 import (
-	"bytes"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"runtime"
-	"time"
 )
 
 type LogLevel int
@@ -25,6 +23,7 @@ const (
 
 const _format = "[%5s] %-20s - %s%s"
 
+// A hook intercepts log message and perform certain tasks, like sending email
 type Hook interface {
 	// Action performed by the Hook.
 	Action(l LogLevel, fileline, msg string)
@@ -32,50 +31,21 @@ type Hook interface {
 	Match(l LogLevel, format string, args ...interface{}) bool
 }
 
-// A simple hook that copies messages from a certain level.
-type SimpleHook struct {
-	LogLevel
-	*bytes.Buffer
-}
-
-// Matches only the log level.
-func (s *SimpleHook) Match(l LogLevel, format string, args ...interface{}) bool {
-	return l == s.LogLevel
-}
-
-// Writes log content in a buffer.
-func (s *SimpleHook) Action(l LogLevel, fileline, msg string) {
-	s.WriteString(fmt.Sprintf("%s "+_format, time.Now().Format("2006/01/02 15:04:05"), levels[int(l)], fileline, "", msg))
-}
-
 var levels = []string{"TRACE", "DEBUG", "INFO", "WARN", "ERROR", "PANIC"}
 
-type Logger interface {
-	Clone(string) Logger
-	Add(Hook)
-	Release(Hook)
-	Output(int, LogLevel, string, ...interface{})
-	Trace(string, ...interface{})
-	Debug(string, ...interface{})
-	Info(string, ...interface{})
-	Warn(string, ...interface{})
-	Error(string, ...interface{})
-	Panic(string, ...interface{})
-}
-
 // Creates a Logger that uses standard output.
-func Std(level LogLevel) Logger {
-	l := logger{log.New(os.Stdout, "", log.LstdFlags), level, "", nil}
+func Std(level LogLevel) *Logger {
+	l := Logger{log.New(os.Stdout, "", log.LstdFlags), level, "", nil}
 	return &l
 }
 
 // Creates a custom Logger.
-func New(log *log.Logger, level LogLevel) Logger {
-	l := logger{log, level, "", nil}
+func New(log *log.Logger, level LogLevel) *Logger {
+	l := Logger{log, level, "", nil}
 	return &l
 }
 
-type logger struct {
+type Logger struct {
 	log    *log.Logger
 	level  LogLevel
 	prefix string
@@ -83,8 +53,11 @@ type logger struct {
 }
 
 // Creates a copy of the logger with the given prefix.
-func (l *logger) Clone(prefix string) Logger {
+func (l *Logger) Clone(prefix string, cleanHooks bool) *Logger {
 	newl := *l
+	if cleanHooks {
+		newl.hooks = nil
+	}
 	if prefix != "" {
 		newl.prefix = prefix + " "
 	}
@@ -92,57 +65,62 @@ func (l *logger) Clone(prefix string) Logger {
 }
 
 // Adds a Hook to the logger.
-func (l *logger) Add(h Hook) {
+func (l *Logger) Add(h Hook) {
 	l.hooks = append(l.hooks, h)
 }
 
 // Release an Hook from the logger.
-func (l *logger) Release(h Hook) {
-	for i := range l.hooks {
-		if l.hooks[i] != h {
+func (l *Logger) Release(h Hook) {
+	for i, hook := range l.hooks {
+		if hook != h {
 			continue
 		}
-		l.hooks = l.hooks[:i+copy(l.hooks[i:], l.hooks[i+1:])]
+		l.hooks[i], l.hooks = l.hooks[len(l.hooks)-1], l.hooks[:len(l.hooks)-1]
 	}
 }
 
 // Writes the log with TRACE level.
-func (l *logger) Trace(format string, args ...interface{}) {
-	l.Output(1, L_TRACE, format, args...)
+func (l *Logger) Trace(format string, args ...interface{}) {
+	l.output(1, L_TRACE, format, args...)
 }
 
 // Writes the log with DEBUG level.
-func (l *logger) Debug(format string, args ...interface{}) {
-	l.Output(1, L_DEBUG, format, args...)
+func (l *Logger) Debug(format string, args ...interface{}) {
+	l.output(1, L_DEBUG, format, args...)
 }
 
 // Writes the log with INFO level.
-func (l *logger) Info(format string, args ...interface{}) {
-	l.Output(1, L_INFO, format, args...)
+func (l *Logger) Info(format string, args ...interface{}) {
+	l.output(1, L_INFO, format, args...)
 }
 
 // Writes the log with WARN level.
-func (l *logger) Warn(format string, args ...interface{}) {
-	l.Output(1, L_WARN, format, args...)
+func (l *Logger) Warn(format string, args ...interface{}) {
+	l.output(1, L_WARN, format, args...)
 }
 
 // Writes the log with ERROR level.
-func (l *logger) Error(format string, args ...interface{}) {
-	l.Output(1, L_ERROR, format, args...)
+func (l *Logger) Error(format string, args ...interface{}) {
+	l.output(1, L_ERROR, format, args...)
 }
 
 // Writes the log with PANIC level.
-func (l *logger) Panic(format string, args ...interface{}) {
-	l.Output(1, L_PANIC, format, args...)
+func (l *Logger) Panic(format string, args ...interface{}) {
+	l.output(1, L_PANIC, format, args...)
 }
 
 // Writes the log with custom level and depth.
-func (l *logger) Output(depth int, lvl LogLevel, format string, args ...interface{}) {
+func (l *Logger) output(depth int, lvl LogLevel, format string, args ...interface{}) {
 	if l.level > lvl {
 		return
 	}
 	_, file, line, _ := runtime.Caller(1 + depth)
 	fileline := fmt.Sprintf("%s:%d", filepath.Base(file), line)
+	for i := range args {
+		if fn, ok := args[i].(func() string); ok {
+			args[i] = fn()
+		}
+	}
 	msg := fmt.Sprintf(format, args...)
 	for _, h := range l.hooks {
 		if h.Match(lvl, format, args...) {
@@ -152,34 +130,34 @@ func (l *logger) Output(depth int, lvl LogLevel, format string, args ...interfac
 	l.log.Output(0, fmt.Sprintf(_format, levels[int(lvl)], fileline, l.prefix, msg))
 }
 
-var defaultLogger = &logger{log.New(os.Stdout, "", log.LstdFlags), 2, "", nil}
+var defaultLogger = &Logger{log.New(os.Stdout, "", log.LstdFlags), 2, "", nil}
 
 // Writes the default log with TRACE level.
 func Trace(format string, args ...interface{}) {
-	defaultLogger.Output(1, L_TRACE, format, args...)
+	defaultLogger.output(1, L_TRACE, format, args...)
 }
 
 // Writes the default log with DEBUG level.
 func Debug(format string, args ...interface{}) {
-	defaultLogger.Output(1, L_DEBUG, format, args...)
+	defaultLogger.output(1, L_DEBUG, format, args...)
 }
 
 // Writes the default log with INFO level.
 func Info(format string, args ...interface{}) {
-	defaultLogger.Output(1, L_INFO, format, args...)
+	defaultLogger.output(1, L_INFO, format, args...)
 }
 
 // Writes the default log with WARN level.
 func Warn(format string, args ...interface{}) {
-	defaultLogger.Output(1, L_WARN, format, args...)
+	defaultLogger.output(1, L_WARN, format, args...)
 }
 
 // Writes the default log with ERROR level.
 func Error(format string, args ...interface{}) {
-	defaultLogger.Output(1, L_ERROR, format, args...)
+	defaultLogger.output(1, L_ERROR, format, args...)
 }
 
 // Writes the default log with PANIC level.
 func Panic(format string, args ...interface{}) {
-	defaultLogger.Output(1, L_PANIC, format, args...)
+	defaultLogger.output(1, L_PANIC, format, args...)
 }
--- console_test.go
+++ console_test.go
@@ -2,20 +2,80 @@ package console
 
 import (
 	"bytes"
+	"log"
+	"os"
 	"testing"
 )
 
-func TestConsole(t *testing.T) {
-	l := Std(L_TRACE)
-	var logFunc = func(s string) {
-		l.Output(1, L_WARN, s)
-	}
+// A simple hook that copies in a buffer messages from a certain level.
+type SimpleHook struct {
+	LogLevel
+	*bytes.Buffer
+}
+
+// Matches only the log level.
+func (s *SimpleHook) Match(l LogLevel, format string, args ...interface{}) bool {
+	return l == s.LogLevel
+}
+
+// Writes log content in a buffer.
+func (s *SimpleHook) Action(l LogLevel, fileline, msg string) {
+	s.WriteString(msg)
+}
+
+// Testing default functions
+func TestDefault(t *testing.T) {
+	Trace("_")
+	Debug("_")
+	Info("_")
+	Error("_")
+	Warn("_")
+	Panic("_")
+}
+
+// Testing a hook with level error and a function argument
+func TestHook(t *testing.T) {
 	s := SimpleHook{L_ERROR, bytes.NewBuffer(nil)}
+	l := Std(L_TRACE)
 	l.Add(&s)
-	l.Output(0, 0, "a")
-	l.Info("some text %q", "a")
-	logFunc("text")
-	l.Clone("[prefix]").Info("some text %q", "a")
+	// message added to hook
 	l.Error("message - %s", "args")
-	t.Log(s.String())
+	// message to a cloned log ignored
+	l.Clone("[prefix]", true).Error("some text %q", "a")
+	// ignored for diffent level
+	l.Panic("not added")
+	l.Warn("not added")
+	l.Info("not added")
+	l.Debug("not added")
+	l.Trace("not added")
+	if s.String() != "message - args" {
+		t.Error("Unexpected string")
+	}
+}
+
+func TestFunction(t *testing.T) {
+	s := SimpleHook{L_INFO, bytes.NewBuffer(nil)}
+	l := Std(L_TRACE)
+	l.Add(&s)
+	l.Info("%s", func() string { return "x_x_x" })
+	if s.String() != "x_x_x" {
+		t.Error("Unexpected string")
+	}
+}
+
+func TestHookRelease(t *testing.T) {
+	s := SimpleHook{L_INFO, bytes.NewBuffer(nil)}
+	s2 := SimpleHook{L_INFO, bytes.NewBuffer(nil)}
+	l := New(log.New(os.Stdout, "", log.LstdFlags), L_TRACE)
+	l.Add(&s)
+	l.Add(&s2)
+	l.Trace("%s", func() string { return "x_x_x" })
+	l.Release(&s)
+	if l := len(l.hooks); l != 1 {
+		t.Error("Failed", l)
+	}
+	l.Release(&s2)
+	if l := len(l.hooks); l != 0 {
+		t.Error("Failed", l)
+	}
 }
